945d023142ea06e0025e28fb37a803ffec9bdfb7
FelipePaulino <felipepaulino08@hotmail.com>
2022-12-12 13:51:32 -0300

    save branch

--- components/SectionCharacters/index.tsx
+++ components/SectionCharacters/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
-// import Api from '../../src/services/api';
+import Api from '../../src/services/api';
 import * as S from './styles'
 
 
 const SectionCharacters: React.FC = () => {
     
-    // useEffect(() => {
-    //     Api.get('/Characters')
-    //     .then(response => console.log(response.data.data))
-    //     .catch(err => console.log(err, "deu errado"));
-    // }, [])
+    useEffect(() => {
+        Api.get('/Characters')
+        .then(response => console.log(response.data.data))
+        .catch(err => console.log(err, "deu errado"));
+    }, [])
     
     return (
         <S.Hello>hello</S.Hello>
